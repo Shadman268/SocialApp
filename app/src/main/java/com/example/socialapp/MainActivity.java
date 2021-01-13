e2b9d3232952c0e5f6cb6e34bb15141519acb2db
shadman <shadmansutshob@gmail.com>
2021-01-13 11:39:34 +0600

    image is added

--- app/src/main/java/com/example/socialapp/MainActivity.java
+++ app/src/main/java/com/example/socialapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.socialapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 
@@ -19,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu,menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.action_add)
+        {
+            startActivity(new Intent(MainActivity.this,PostActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/socialapp/PostActivity.java
+++ app/src/main/java/com/example/socialapp/PostActivity.java
@@ -1,14 +1,47 @@
 package com.example.socialapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton mSelectImage;
+    private static final  int galleryReq = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mSelectImage = (ImageButton) findViewById(R.id.ImageButtonId);
+       mSelectImage.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+               galleryIntent.setType("image/*");
+               startActivityForResult(galleryIntent,galleryReq);
+
+
+           }
+       });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==galleryReq && resultCode==RESULT_OK)
+        {
+            Uri Imageuri = data.getData();
+            mSelectImage.setImageURI(Imageuri);
+
+        }
     }
 }
